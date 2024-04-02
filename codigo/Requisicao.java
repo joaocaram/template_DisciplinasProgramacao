23624236a9d3a2e537b7f13d9ca1d9092baaa1ff
Roberta Sophia <lobelta007@gmail.com>
2024-04-02 17:33:50 -0300

    classe restaurante #5

---
+++ codigo/Requisicao.java
@@ -0,0 +1,12 @@
+public class Requisicao {
+    private double quantidadePessoas;
+    private double numRequisicao;
+
+    public double getQuantidadePessoas(){
+        return quantidadePessoas;
+    }
+
+    public double getNumRequisicao(){
+        return numRequisicao;
+    }
+}
