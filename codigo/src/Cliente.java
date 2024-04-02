8bd45f84ad9f8b945d04f7af689d3d1495787438
Arthur Kramberger <89984310+arthurcuri@users.noreply.github.com>
2024-04-02 09:21:44 -0300

    Add files via upload

--- codigo/src/Cliente.java
+++ codigo/src/Cliente.java
@@ -1,5 +1,24 @@
 public class Cliente {
+    
+    public String nome;
+    public int idCliente;
 
-     private String nome;  
-     private int idCliente;
+    public void statusCliente(){
+        getIdCliente();
+        getNome();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    
 }
