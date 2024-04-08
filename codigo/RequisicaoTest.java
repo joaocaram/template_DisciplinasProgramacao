1e2aee9714ff79351975c61dab198634da43b40d
JoaoPedroGRibeiro <144904799+JoaoPedroGRibeiro@users.noreply.github.com>
2024-04-08 09:51:10 -0300

    Create RequisicaoTest.java

---
+++ codigo/RequisicaoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RequisicaoTest {
+
+    @Test
+    public void testConstrutor() {
+        Requisicao.getnumDaFila(2);
+        assertEquals(numDaFila.getNumDaFila(), 2);
+
+    }
+}
