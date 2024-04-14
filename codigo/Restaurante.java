705421c31d99ddb5c8626d5c8ad5b62331d987d8
Eduardo Palhares Reale Pereira <130712646+EduardoPalharesRealePereira@users.noreply.github.com>
2024-04-14 18:59:35 -0300

    Update Restaurante.java

--- codigo/Restaurante.java
+++ codigo/Restaurante.java
@@ -1,47 +1,64 @@
-import java.util.List;
-import java.util.Queue;
-
 public class Restaurante {
-    private int mesas;
-    private Queue<Cliente> filaEspera;
+  private List<Mesa> mesas;
+  private Queue<Requisicao> filaEspera;
+  private List<Requisicao> requisicoesAtendidas;
 
-    public Restaurante(int mesas, Queue<Cliente> filaEspera) {
-        this.mesas = mesas;
-        this.filaEspera = filaEspera;
+  public Restaurante() {
+    mesas = new ArrayList<>();
+    filaEspera = new LinkedList<>();
+    requisicoesAtendidas = new ArrayList<>();
+  }
+
+  public boolean alocarMesa(Requisicao requisicao) {
+    for (Mesa mesa : mesas) {
+      if (mesa.verificarDisponibilidade() && mesa.getCapacidade() >= requisicao.getNumPessoas()) {
+        mesa.setIdRequisicao(requisicao.getIdRequisicao());
+        requisicoesAtendidas.add(requisicao);
+        filaEspera.remove(requisicao);
+        System.out.println("Requisição deu bom: " + requisicao.getIdRequisicao());
+        System.out.println("Requisição atendida com sucesso: " + requisicao.getIdRequisicao());
+        return true;
+      }
     }
+    adicionarClienteFilaEspera(requisicao);
+    System.out.println("Requisição deu ruim: " + requisicao.getIdRequisicao());
+    System.out.println("Requisição adicionada à fila de espera: " + requisicao.getIdRequisicao());
+    return false;
+  }
 
-    public boolean alocarMesa() {
-        if (mesas > 0) {
-            mesas--;
-            return true;
-        } else {
-            return false;
+  public void liberarMesa(int idRequisicao) {
+    for (Mesa mesa : mesas) {
+      if (mesa.getIdRequisicao() == idRequisicao) {
+        Requisicao.encerrarRequisicao();
+        for (Requisicao requisicao : requisicoesAtendidas) {
+          if (requisicao.getIdRequisicao() == idRequisicao) {
+            requisicao.encerrarRequisicao();
+          }
         }
+        mesa.setIdRequisicao(-1);
+        if (filaEspera.isEmpty()) {
+          return;
+        if (!filaEspera.isEmpty()) {
+          alocarMesa(proxNaFila());
+        }
+        alocarMesa(proxNaFila());
+        return;
+      }
     }
-
-    public int getMesas() {
-        return mesas;
+  }
+  public int verificarMesasDisponiveis() {
+    int disponiveis = 0;
+    for (Mesa mesa : mesas) {
+      if (mesa.verificarDisponibilidade()) {
+        System.out.println("Mesa disponível: " + mesa.getIdMesa());
+        System.out.println("Capacidade: " + mesa.getCapacidade());
+        disponiveis++;
+      }
     }
-
-    public void setMesas(int mesas) {
-        this.mesas = mesas;
-    }
-
-    public int getFilaEsperaSize() {
-        return filaEspera.size();
-    }
-
-    public boolean isFilaEsperaEmpty() {
-        return filaEspera.isEmpty();
-    }
-
-    public void adicionarClienteFilaEspera(Cliente cliente) {
-        filaEspera.add(cliente);
-    }
-
-    public Cliente proxNaFila() {
-        return filaEspera.peek();
-    }
-
-    // Outros métodos, getters e setters conforme necessário
-}
+    return disponiveis;
+  }
+  public void adicionarClienteFilaEspera(Requisicao requisicao) {
+    filaEspera.add(requisicao);
+  }
+  public Requisicao proxNaFila() {
+    return filaEspera.poll();
