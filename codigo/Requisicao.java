33b48a30879a0c18dfcf78ac8f5de1fce5e950ad
Saulo <143658597+SaulimDoPvP@users.noreply.github.com>
2024-04-15 08:46:38 -0300

    Comentários na requisição #8

--- codigo/Requisicao.java
+++ codigo/Requisicao.java
@@ -1,23 +1,26 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+// Classe que representa uma requisição para reservar uma mesa
 public class Requisicao {
-    private Cliente cliente;
-    private int quantidadePessoas;
-    private LocalDate data;
-    private LocalTime horaEntrada;
-    private LocalTime horaSaida;
+    private Cliente cliente; // Cliente que está fazendo a requisição
+    private int quantidadePessoas; // Quantidade de pessoas para a reserva
+    private LocalDate data; // Data da reserva
+    private LocalTime horaEntrada; // Hora de entrada na reserva
+    private LocalTime horaSaida; // Hora de saída da reserva (pode ser nulo se ainda não saiu)
 
+    // Construtor da classe
     public Requisicao(Cliente cliente) {
         this.cliente = cliente;
-        this.data = LocalDate.now();
-        this.horaEntrada = LocalTime.now();
-        this.horaSaida = null;
+        this.data = LocalDate.now(); // Define a data atual como a data da reserva
+        this.horaEntrada = LocalTime.now(); // Define a hora atual como a hora de entrada na reserva
+        this.horaSaida = null; // Ainda não há hora de saída definida
     }
 
+    // Métodos get que obtem os atributos da classe
     public LocalTime getHoraEntrada() {
         return horaEntrada;
-    }
+    }   
 
     public LocalDate getData() {
         return data;
@@ -35,6 +38,7 @@ public class Requisicao {
         return quantidadePessoas;
     }
 
+    // Métodos sets que definem os atributos da classe
     public void setData(LocalDate data) {
         this.data = data;
     }
@@ -47,7 +51,9 @@ public class Requisicao {
         this.horaSaida = horaSaida;
     }
 
+    // Método para requerir uma mesa com uma quantidade específica de pessoas
     public void requerirMesa(int quantidadePessoas) {
+        // Verifica se a quantidade de pessoas é válida (entre 1 e 8)
         if (quantidadePessoas > 0 && quantidadePessoas <= 8) {
             this.quantidadePessoas = quantidadePessoas;
         } else {
