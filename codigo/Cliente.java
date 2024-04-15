aadc74a545447cc65e1cb986544673189bd97c97
gabitolage <gglage11@gmail.com>
2024-04-15 17:22:37 -0300

    atualização cliente

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,14 +1,20 @@
 
 public class Cliente {
     private String nome;
+    private int id;
 
     public Cliente(String nome) {
         this.nome = nome;
+        this.id = id;
 
     }
 
     public String getNome() {
         return nome;
     }
+    public int getId() {
+        return id
+    }
 
 }
+
