ceb41243475f4261dc3298de1f02f9c243107f36
hegdt <143658544+hegdt@users.noreply.github.com>
2024-04-22 08:21:37 -0300

    Create Manu.java

---
+++ codigo/Manu.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private static List<Produto> produtos;
+
+    public Menu() {
+        produtos = new ArrayList<>();
+    }
+
+    public void adicionarProduto(String nome, Integer preco) {
+        Produto novoProduto = new Produto(nome, preco);
+        produtos.add(novoProduto);
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("Menu do Restaurante:");
+        for (int i = 0; i < produtos.size(); i++) {
+            System.out.println((i+1) + ". " + produtos.get(i).getNome() + " - R$" + produtos.get(i).getPreco());
+        }
+    }
+
+    public Produto getProduto(int indice) {
+        return produtos.get(indice);
+    }
+
+    public int tamanhoMenu() {
+        return produtos.size();
+    }
+}
