ad21776eb5c20e98889a0012a487e0eb2b96ba86
lucaslmchaves <lucaslmchaves@gmail.com>
2024-05-12 22:11:40 -0300

    fix: mesa and requisicao controllers

--- src/main/java/com/advanced/comidinhasveganas/controllers/RequisicaoController.java
+++ src/main/java/com/advanced/comidinhasveganas/controllers/RequisicaoController.java
@@ -1,3 +1,31 @@
+package com.advanced.comidinhasveganas.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.lpm.models.Requisicao;
+import com.lpm.services.RequisicaoServices;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+
 @RestController
 @RequestMapping("/requisicoes")
 public class RequisicaoController {
