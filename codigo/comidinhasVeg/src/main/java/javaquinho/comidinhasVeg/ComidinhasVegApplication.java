4cdc795b75e6fde4e1a779666ac381a043fa2d69
Eduardo <eduardo.henriquems@gmail.com>
2024-05-13 09:30:17 -0300

    adicionando spring #13

---
+++ codigo/comidinhasVeg/src/main/java/javaquinho/comidinhasVeg/ComidinhasVegApplication.java
@@ -0,0 +1,13 @@
+package javaquinho.comidinhasVeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComidinhasVegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComidinhasVegApplication.class, args);
+	}
+
+}
---
+++ codigo/comidinhasVeg/src/test/java/javaquinho/comidinhasVeg/ComidinhasVegApplicationTests.java
@@ -0,0 +1,13 @@
+package javaquinho.comidinhasVeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidinhasVegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
