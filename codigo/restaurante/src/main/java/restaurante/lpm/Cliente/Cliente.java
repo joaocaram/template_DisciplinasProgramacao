5ad3327bb0e3b93cf3fc3c7c5a9ef94f5f92d4cd
Pedro Lobato <pedrolfirbida@gmail.com>
2024-05-13 20:20:03 -0300

    Update Cliente.java

--- codigo/restaurante/src/main/java/restaurante/lpm/Cliente/Cliente.java
+++ codigo/restaurante/src/main/java/restaurante/lpm/Cliente/Cliente.java
@@ -1,23 +1,28 @@
-public class Cliente {
-    private int id;
-    private String nome;
-    private String pedido;
-    private int quantidade;
+import org.springframework.web.bind.annotation.*;
 
-    public Cliente(int id, String nome) {
-        this.id = id;
-        this.nome = nome;
-        this.pedido = "";
-        this.quantidade = 0;
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @PostMapping("/criar")
+    public Cliente criarCliente(@RequestParam("nome") String nome) {
+        return new Cliente(nome);
     }
 
-    public void fazerPedido(String pedido) {
-        this.pedido = pedido;
-        System.out.println(nome + " fez o pedido: " + pedido);
+    @GetMapping("/{id}")
+    public Cliente getCliente(@PathVariable("id") int id) {
+    }
+}
+
+@RestController
+@RequestMapping("/pedido")
+public class PedidoController {
+
+    @PostMapping("/criar")
+    public Pedido criarPedido(@RequestBody Pedido pedido) {
     }
 
-    public void declararQuantidade(int quantidade) { 
-        this.quantidade = quantidade;
-        System.out.println(nome + " declarou: " + quantidade + " Convidados");
+    @GetMapping("/{id}")
+    public Pedido getPedido(@PathVariable("id") int id) {
     }
 }
