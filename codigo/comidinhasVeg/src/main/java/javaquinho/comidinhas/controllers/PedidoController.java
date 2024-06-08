0985e17aa51d7484c55146df7daf6078579c34e1
iannovais <iandosreisnovais@gmail.com>
2024-06-08 10:11:07 -0300

    Corrigido método GET de pedido #14

--- codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/controllers/PedidoController.java
+++ codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/controllers/PedidoController.java
@@ -1,30 +1,41 @@
 package javaquinho.comidinhas.controllers;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 import javaquinho.comidinhas.models.Pedido;
 import javaquinho.comidinhas.repositories.PedidoRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pedidos")
+@Validated
 public class PedidoController {
 
-    private final PedidoRepository pedidoRepository;
-
     @Autowired
-    public PedidoController(PedidoRepository pedidoRepository) {
-        this.pedidoRepository = pedidoRepository;
-    }
+    private PedidoRepository pedidoRepository;
 
-    @PostMapping
-    public Pedido criarPedido(@RequestBody Pedido pedido) {
-        return pedidoRepository.save(pedido);
+    @GetMapping
+    public ResponseEntity<List<Pedido>> getAllPedidos() {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        return ResponseEntity.ok().body(pedidos);
     }
 
     @GetMapping("/{id}")
-    public Pedido buscarPedido(@PathVariable Long id) {
-        return pedidoRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Pedido não encontrado"));
+    public ResponseEntity<Pedido> getPedido(@PathVariable Long id) {
+        Optional<Pedido> pedido = pedidoRepository.findById(id);
+        return pedido.isPresent() ? ResponseEntity.ok(pedido.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
+        Pedido novoPedido = pedidoRepository.save(pedido);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}").buildAndExpand(novoPedido.getId()).toUri();
+        return ResponseEntity.created(uri).body(novoPedido);
     }
 }
